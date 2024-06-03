64a52291075fb2989a62115133b0efad8d7c10ee
SantiagoAlfonso992 <sa276280@fi365.ort.edu.uy>
2024-06-03 19:09:16 -0300

    add getAll

--- src/app/todos.service.ts
+++ src/app/todos.service.ts
@@ -1,9 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { TodoReturnModel } from './returnedModels';
+
+export interface ITodosService {
+  getAllTodos(): Observable<TodoReturnModel[]>;
+ }
 
 @Injectable({
   providedIn: 'root',
 })
-export class TodosService {
+export class TodosService implements ITodosService{
   constructor(private http: HttpClient) {}
+
+  getAllTodos(): Observable<TodoReturnModel[]>{
+    return this.http.get<any>(`https://jsonplaceholder.typicode.com/todos`);
+  }
 }
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TodosService } from '../todos.service';
+import { TodoReturnModel } from '../returnedModels';
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.scss'],
 })
-export class TodosComponent {
+export class TodosComponent implements OnInit {
   constructor(private todosService: TodosService) {}
+  todos : TodoReturnModel[] = []
 
-  randomMethod() {}
+  ngOnInit(): void {
+    this.getAllTodos()
+  }
+
+  randomMethod(){
+
+  }
+
+  getAllTodos() {
+    this.todosService.getAllTodos().subscribe((response) => {
+      this.todos = response;
+    });
+  }
 }
